26a35ccb237ccc3a92dcf4b346f4c00473452310
souravsinghnegi <souravsinghnegiuk@gmail.com>
2025-08-25 21:32:13 +0530

    ipgs

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -21,4 +21,29 @@ const Home = () => {
             in modern Istanbul embarks on a quest to save the city from an
             immortal enemy.
           </p>
+          <div className="hero-btns">
+            <button className="btn">
+              <img src={play_icon} alt="" />
+              Play
+            </button>
+            <button className="btn dark-btn">
+              <img src={info_icon} alt="" />
+              More Info
+            </button>
+          </div>
+          <TitleCards />
+        </div>
+      </div>
+      <div className="more-cards">
+        <TitleCards title={"Blockbuster Movies"} category={"top_rated"} />
+        <TitleCards title={"Only on Netflix"} category={"popular"} />
+        <TitleCards title={"Upcoming"} category={"upcoming"} />
+        <TitleCards title={"Top Pics for You"} category={"now_playing"} />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
           
